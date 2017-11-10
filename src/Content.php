355aa60413e02f83b9cab4629460585c21da15b1
papparazzo <pappi-@gmx.de>
2017-11-10 22:20:28 +0100

    content added

---
+++ src/Content.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2017  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+namespace SFW2;
+
+class Content {
+
+    protected $cssFiles     = array();
+    protected $jsFiles      = array();
+
+
+    public function addJSFile($file) {
+        $this->jsFiles[$file] = $file;
+    }
+
+    public function addCSSFile($file) {
+        $this->cssFiles[$file] = $file;
+    }
+
+
+    public function getJSFiles() {
+        return $this->jsFiles;
+    }
+
+    public function getCSSFiles() {
+        return $this->cssFiles;
+    }
+
+
+
+}
