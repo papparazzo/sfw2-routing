f1412df37ac927e1a4ef31836ca0cd0f22358dec
papparazzo <pappi-@gmx.de>
2017-11-27 00:13:25 +0100

    wip...

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -22,82 +22,40 @@
 
 namespace SFW2\Routing;
 
-use SFW2\Routing\Dispatcher\Handler\HTML;
-use SFW2\Core\View;
-
 class Dispatcher {
 
     /**
-     * @var Content
+     * @var \SFW2\Routing\Request
      */
-    public $content = '';
+    public $request = null;
 
-    public function __construct(Content $content) {
-        $this->content = $content;
+    public function __construct(Request $request) {
+        $this->request = $request;
     }
 
-    protected function dispatchRequestType() {
+    public function dispatch(Result $result) {
+        if($result instanceof Result\File) {
 
-        if(is_file($this->content)) {
-            return new Dispatcher\Handler\FileDownload();
         }
-        if(isset($this->server['HTTP_X_REQUESTED_WITH']) && !is_array($this->content)) {
-            return new Dispatcher\Handler\XML($this->registry, $data);
-        }
-        if(isset($this->server['HTTP_X_REQUESTED_WITH']) && is_array($this->content)) {
-            return new Dispatcher\Handler\Json($this->registry, $data);
-        }
-        return new HTML($this->registry, $data);
-    }
+        
 
-    public function handleXML() {
-        header('Content-type: text/xml');
-        echo '<?xml version="1.0" encoding="utf-8"?>';
-        echo $this->container['content'];
-    }
 
-    public function handleJSON() {
-        if(!isset($this->container['content']['error'])) {
-            $this->container['content'] = ['error' => false];
-        }
-        header('Content-type: application/json');
-        echo json_encode($this->container['content']);
-    }
 
-    public function handleHTML() {
-        $view = new View();
-        $view->appendCSSFiles($this->container['cssfiles']);
-        $view->appendJSFiles($this->container['jsfiles']);
+        switch($this->request->getRequestType()) {
+            case Request::REQUEST_TYPE_AJAX_JSON:
+                return new ResponseType\Json();
 
-        $view->assign('content',       $this->container['content']);
-        $view->assign('title',         $this->container['title']);
-        $view->assign('menu',          $this->container['menu']);
-        $view->assign('authenticated', $this->container['authenticated']);
-        $view->showContent($this->config->getTemplateFile('olframe'));
-    }
+            case Request::REQUEST_TYPE_AJAX_XML:
+                return new ResponseType\Xml();
 
-    public function handleFile() {
-        $file = $this->data['path'] . $this->data['fileName'];
-
-        if(!file_exists($file)) {
-            return false;
+            case Request::REQUEST_TYPE_HTML:
+                return new ResponseType\Html();
         }
 
-        $finfo = finfo_open(FILEINFO_MIME);
-        $mimetype = finfo_file($finfo, $file);
-        finfo_close($finfo);
-        header('Pragma: public');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Cache-Control: private', false);
-        header('Content-Type: ' . $mimetype);
-        header('Content-Disposition: attachment; filename="' . $this->data['Name'] . '";');
-        header('Content-Transfer-Encoding: binary');
-        header('Content-Length: ' . filesize($file));
-        readfile($file);
 
-        if($this->data['unlink']) {
-            unlink($file);
-        }
+
+
+        $this->result = $result;
+var_dump($result);
     }
 }
