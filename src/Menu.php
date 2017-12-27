5fda8fc6da0cf0c4a4f8e1fb3e329a958aa7b213
papparazzo <pappi-@gmx.de>
2017-12-27 23:40:59 +0100

    wip...

--- src/Menu.php
+++ src/Menu.php
@@ -51,38 +51,9 @@ class Menu {
 }
 
 /*
- * <?php
-
-/**
- *  SFW - SimpleFrameWork
- *
- *  Copyright (C) 2015  Stefan Paproth
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
- *
  */
 
-namespace SFW;
-
-class Menu {
-
-    protected $menu      = array();
-
-    /**
-     * @var \SFW\Database
-     */
-    protected $db        = null;
+class Menu5 {
 
     public function __construct(Database $db) {
         $this->db     = $db;
