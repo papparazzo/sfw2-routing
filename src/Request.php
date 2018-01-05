d9a7402136af630ca7c33130e6861b7eb8a01476
papparazzo <pappi-@gmx.de>
2018-01-06 00:22:44 +0100

    path fixed

--- src/Request.php
+++ src/Request.php
@@ -90,7 +90,7 @@ class Request {
         $path = mb_substr($path, 1);
         $path = strtolower($path);
         $path = preg_replace('#[^A-Za-z0-9/]#', '', $path);
-        $path = rtrim($path, '/');
+        $path = '/' . trim($path, '/');
         return $path;
     }
 }
