b2a467f375db7f97aec115ab17426777609643e8
papparazzo <pappi-@gmx.de>
2023-09-12 18:07:01 +0200

    class added

---
+++ src/RequestHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SFW2\Routing;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class RequestHandler implements RequestHandlerInterface {
+
+    private MiddlewareInterface $middleware;
+
+    private RequestHandlerInterface $next;
+
+    public function __construct(MiddlewareInterface $middleware, RequestHandlerInterface $next) {
+        $this->middleware = $middleware;
+        $this->next = $next;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface {
+        return $this->middleware->process($request, $this->next);
+    }
+}
