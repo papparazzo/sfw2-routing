40d12c5658f0130196a70803a16a3243c98dc005
papparazzo <pappi-@gmx.de>
2023-09-12 17:52:30 +0200

    Dispatcher added

---
+++ src/Dispatcher.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SFW2\Routing;
+
+use Psr\Http\Message\ResponseInterface;
+
+class Dispatcher
+{
+    public function dispatch(ResponseInterface $response): void
+    {
+        if (headers_sent() === false) {
+            $this->emitHeaders($response);
+        }
+
+        $this->emitBody($response);
+    }
+
+    private function emitHeaders(ResponseInterface $response): void
+    {
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header("$name: $value", false);
+            }
+        }
+    }
+
+    private function emitBody(ResponseInterface $response): void
+    {
+        $body = $response->getBody();
+        if ($body->isSeekable()) {
+            $body->rewind();
+        }
+
+        while (!$body->eof() && connection_status() == CONNECTION_NORMAL) {
+            echo $body->read(4096);
+        }
+    }
+}
