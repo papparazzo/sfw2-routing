572607569b9427c4c8eb8920a16a0c06a8257ce1
papparazzo <pappi-@gmx.de>
2023-10-04 06:41:07 +0200

    new classes added

---
+++ src/ResponseEngine.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2018  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+namespace SFW2\Routing;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use SFW2\Routing\Render\RenderInterface;
+
+class ResponseEngine {
+
+    public function __construct(
+        private readonly RenderInterface $renderEngine,
+        private readonly ResponseFactoryInterface $responseFactory
+    )
+    {
+    }
+    public function render(Request $request, string $template, array $data): Response {
+        $response = $this->responseFactory->createResponse();
+        return $this->renderEngine->render($request, $response, $template, $data);
+    }
+}
