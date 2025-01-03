4dcd446875666fa4bd9bf9c807df7580837b7b7c
papparazzo <pappi-@gmx.de>
2025-01-03 10:01:13 +0100

    Condition added

--- src/Render/RenderComposite.php
+++ src/Render/RenderComposite.php
@@ -4,30 +4,26 @@ namespace SFW2\Routing\Render;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use SFW2\Routing\Render\Conditions\ConditionInterface;
 
 class RenderComposite implements RenderInterface
 {
     /**
-     * @var RenderInterface[]
+     * @var list<array{on: ConditionInterface, render: RenderInterface}>
      */
     protected array $engines = [];
 
-    public function add(RenderInterface $engine): self
+    public function addEngine(ConditionInterface $on, RenderInterface $engine): self
     {
-        $this->engines[] = $engine;
-        return $this;
-    }
-
-    public function addEngines(RenderInterface ...$engines): self
-    {
-        $this->engines = array_merge($this->engines, $engines);
+        $this->engines[] = ['on' => $on, 'render' => $engine];
         return $this;
     }
 
     public function render(Request $request, Response $response, array $data = [], ?string $template = null): Response
     {
-        foreach ($this->engines as $engine) {
-            $response = $engine->render($request, $response, $data, $template);
+        foreach ($this->engines as $entry) {
+            $response =
+                $entry['on']($request) ? $entry['render']->render($request, $response, $data, $template) : $response;
         }
         return $response;
     }
